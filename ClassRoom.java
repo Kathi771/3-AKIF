445e6c0d55411f131a1efeb2c1e7038da2d3cb90
Ekaterine-3AKIF <hah240737@spengergasse.at>
2024-10-06 16:34:49 +0200

    Initial sharing of project

---
+++ ClassRoom.java
@@ -0,0 +1,45 @@
+
+public class ClassRoom
+{
+    private String  roomNr;
+    private boolean computer;
+    private int     size;
+    
+     public void setRoomNr(String newRoomNr)
+    {
+        roomNr = newRoomNr;
+    }
+     public void setComputer(boolean newComputer) 
+    {  
+        computer = newComputer;
+        
+    }
+    public void setSize(int newSize)
+    {
+       size = newSize;
+    }
+    public String getRoomNr()
+    {
+        return roomNr;
+        
+    }
+     public boolean getComputer()
+    {
+        return computer;
+        
+    }
+   
+
+    
+    public int gerSize()
+    {
+     return size;
+    }
+        
+    public int getSize()
+    {
+        return size;
+    }    
+    
+}
+
